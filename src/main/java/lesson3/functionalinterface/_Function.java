024b6c98eff255aadeb69a51894b6bcf2cd54eeb
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-08-21 22:33:51 +0400

    -- combine to Functions

--- src/main/java/lesson3/functionalinterface/_Function.java
+++ src/main/java/lesson3/functionalinterface/_Function.java
@@ -6,9 +6,12 @@ public class _Function {
     public static void main(String[] args) {
         System.out.println(incrementByOne(0));
         System.out.println(incrementByOneFunction.apply(0));
+        System.out.println(incrementByOneFunction.andThen(multiplyBy10Function).apply(1));
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
 
     static int incrementByOne(int number){
         return number+1;
