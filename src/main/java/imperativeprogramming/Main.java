6a59efbe2bf8296a1443aafb013295b7d5359794
rovshan.huseynov <gitlab123!>
2022-08-21 20:41:49 +0400

    -- implement imperative approach

---
+++ src/main/java/imperativeprogramming/Main.java
@@ -0,0 +1,52 @@
+package imperativeprogramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static imperativeprogramming.Main.Gender.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
+        );
+
+        // Imperative approach
+        List<Person> females = new ArrayList<>();
+        for(Person person : people){
+            if(person.gender.equals(FEMALE)){
+                females.add(person);
+            }
+        }
+
+        for(Person female : females){
+            System.out.println(female);
+        }
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender){
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
