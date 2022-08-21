aea2cec4dc9e4610883c3b70544997a2af437d1e
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-08-21 22:55:34 +0400

    -- implement BiFunction

--- src/main/java/lesson3/functionalinterface/_Function.java
+++ src/main/java/lesson3/functionalinterface/_Function.java
@@ -1,5 +1,6 @@
 package lesson3.functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
@@ -7,11 +8,14 @@ public class _Function {
         System.out.println(incrementByOne(0));
         System.out.println(incrementByOneFunction.apply(0));
         System.out.println(incrementByOneFunction.andThen(multiplyBy10Function).apply(1));
+        System.out.println(addTwoIntegersByBiFunction.apply(1,2));
     }
 
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
 
     static Function<Integer, Integer> multiplyBy10Function = number -> number * 10;
+
+    static BiFunction<Integer, Integer, Integer> addTwoIntegersByBiFunction = (a,b) -> a+b;
 
     static int incrementByOne(int number){
         return number+1;
