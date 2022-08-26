fc67ec5eb0489b49153aad54138e7aa3a92cde06
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-08-26 23:56:55 +0400

    -- implement Supplier

---
+++ src/main/java/lesson3/functionalinterface/_Supplier.java
@@ -0,0 +1,16 @@
+package lesson3.functionalinterface;
+
+import java.util.function.Supplier;
+
+public class _Supplier {
+    public static void main(String[] args) {
+        System.out.println(getDBConnectionUrl());
+        System.out.println(getDBConnectionUrlSupplier.get());
+    }
+
+    static String getDBConnectionUrl(){
+        return "jdbc://localhost:5432/users";
+    }
+
+    static Supplier<String> getDBConnectionUrlSupplier = () -> "jdbc://localhost:5432/users";
+}
