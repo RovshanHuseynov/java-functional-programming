d9cdc48ba37e12abf6ba2ccbe304260ea717ebb1
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-08-28 00:05:53 +0400

    -- implement Optionals

---
+++ src/main/java/optionals/Main.java
@@ -0,0 +1,23 @@
+package optionals;
+
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Optional.ofNullable(null)
+                .orElseGet(() -> "default value"));
+
+        System.out.println(Optional.ofNullable("Hello")
+                .orElse("default value"));
+
+        Optional.ofNullable("John@gmail.com")
+                .ifPresent(email -> System.out.println("Sending email to: " + email));
+
+        Optional.ofNullable(null)
+                .ifPresentOrElse(email -> System.out.println("Sending email to: " + email) ,
+                        () -> System.out.println("Cannot send email"));
+
+        System.out.println(Optional.ofNullable(null)
+                .orElseThrow(() -> new IllegalStateException("exception")));
+    }
+}
