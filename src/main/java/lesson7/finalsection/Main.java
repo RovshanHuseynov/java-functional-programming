2dcbc6ef53e3fb0297033e7b6edfabc9b9c49ed4
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-04 13:06:05 +0400

    -- implement Callbacks like JavaScript

--- src/main/java/lesson7/finalsection/Main.java
+++ src/main/java/lesson7/finalsection/Main.java
@@ -1,16 +1,41 @@
 package lesson7.finalsection;
 
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("adasd");
+        hello("Rovshan", null,
+                firstName -> System.out.println("no lastName is provided for " + firstName));
+
+        hello2("Rovshan", null,
+                () -> System.out.println("no last name is provided"));
     }
 
-    /*function hello(firstName, lastName, callback){
-        console.log(firstName);
-        if(lastName){
-            console.log(lastName);
+    static void hello(String firstName, String lastName,
+                      Consumer<String> callback){
+        if(lastName != null){
+            System.out.println(lastName);
         } else {
-            callback();
+            callback.accept(firstName);
+        }
+    }
+
+    static void hello2(String firstName, String lastName, Runnable callback){
+        if(lastName != null){
+            System.out.println(lastName);
+        } else {
+            callback.run();
+        }
+    }
+
+    /*
+    javascript
+    function hello(firstName, lastName, callback){
+        console.log(firstName);
+        if(lastName){  // lastName is provided
+            console.log(lastName);
+        } else {   // no lastName is provided
+            callback(); // callback() is just extra function
         }
     }*/
 }
