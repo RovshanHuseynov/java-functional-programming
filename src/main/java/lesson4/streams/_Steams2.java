646cf4861426a7f473e7fe7b97f643c4522c5e01
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-04 18:18:30 +0400

    -- practise map(), findFist(), filter() methods

--- src/main/java/lesson4/streams/_Steams2.java
+++ src/main/java/lesson4/streams/_Steams2.java
@@ -1,6 +1,7 @@
 package lesson4.streams;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class _Steams2 {
@@ -15,10 +16,22 @@ public class _Steams2 {
         );
 
         List<Person> youngPeople = people.stream()
-                .filter(person -> person.getAge() <= 18)
-                .collect(Collectors.toList());
+                .filter(person -> person.getAge() <= 18)   // intermediate operator
+                .collect(Collectors.toList());             // terminal operator
 
         System.out.println(youngPeople);
+
+        List<String> names = people.stream()
+                .map(person -> person.getName())
+                .collect(Collectors.toList());
+
+        names.stream().forEach(System.out::println);
+
+        Optional<String> firstName = people.stream()
+                .map(person -> person.getName())
+                .findFirst();
+
+        System.out.println(firstName);
     }
 
     static class Person {
