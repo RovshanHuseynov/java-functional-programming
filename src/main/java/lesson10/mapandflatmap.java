2560b71292dfe07186a0263cb0611c9f2ffadc32
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-09-20 23:53:57 +0400

    -- practise map & flatMap

--- src/main/java/lesson10/mapandflatmap.java
+++ src/main/java/lesson10/mapandflatmap.java
@@ -1,9 +1,11 @@
 package lesson10;
 
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class mapandflatmap {
+    // source: https://www.youtube.com/watch?v=sVX421gaRT4
     public static void main(String[] args) {
         List<Integer> list = List.of(2,10,15,1,9);
 
@@ -17,6 +19,21 @@ public class mapandflatmap {
                 .flatMap(i -> Stream.of(i+10, i+20))
                 .forEach(i -> System.out.print(i + ", "));
         System.out.println();
+        System.out.println("----------------------------");
+
+        int[] arr1 = new int[] {1,2,3,4};
+        int[] arr2 = new int[] {10,20,30,40};
+
+        Stream.of(arr1, arr2)
+                .flatMapToInt(i -> IntStream.of(i))
+                .forEach(i -> System.out.print(i + ", "));
+        System.out.println();
+        System.out.println("----------------------------");
+
+        Stream.of(arr1, arr2)
+                .flatMapToInt(IntStream::of)
+                .max()
+                .ifPresent(System.out::println);
         System.out.println("----------------------------");
     }
 }
