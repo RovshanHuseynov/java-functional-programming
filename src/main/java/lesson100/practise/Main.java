21f114da03e1ab48a7e9411cab6440f471057e0a
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-10-04 22:20:00 +0400

    -- practise Collectors.groupingBy() in streams

--- src/main/java/lesson100/practise/Main.java
+++ src/main/java/lesson100/practise/Main.java
@@ -1,8 +1,19 @@
 package lesson100.practise;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
+    // find frequency of each character in a given String
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        Map<String, Long> countMap = Stream.of(input.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(countMap);
     }
 }
