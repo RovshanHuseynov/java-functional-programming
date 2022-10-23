6223f542ac0bf9d7f8693d15bf06f940ab740728
rovshan.huseynov <rovsen.huseyn.oglu@gmail.com>
2022-10-23 14:05:17 +0400

    -- learn how the hashMap works

---
+++ src/main/java/lesson0/hashmap/Employee.java
@@ -0,0 +1,33 @@
+package lesson0.hashmap;
+
+import java.util.Objects;
+
+public class Employee {
+    String name;
+    int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (this == o) return true;
+        if(!(o instanceof Employee)) return false;
+
+        Employee that = (Employee) o;
+
+        return this.age == that.age &&
+                Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + name.hashCode();
+        result = result * 31 + age;
+        return result;
+    }
+}
---
+++ src/main/java/lesson0/hashmap/Main.java
@@ -0,0 +1,41 @@
+package lesson0.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Rovshan", 30);
+        Employee emp2 = new Employee("Rovshan", 30);
+
+        System.out.println(emp1 == emp2);
+        System.out.println(emp1.equals(emp2));
+        System.out.println(emp1);
+        System.out.println(emp2);
+        System.out.println(emp1.hashCode());
+        System.out.println(emp2.hashCode());
+
+        Map<Employee, String> map = new HashMap<>();
+        map.put(new Employee("Rovshan", 30), "first");
+        System.out.println(map.get(new Employee("Rovshan", 30)));
+
+        /* results when hashCode method is overriden
+        false
+        true
+        lesson0.hashmap.Employee@5d578d4
+        lesson0.hashmap.Employee@5d578d4
+        97876180
+        97876180
+        first
+         */
+        /* results when hashCode method is NOT overriden
+        false
+        true
+        lesson0.hashmap.Employee@2133c8f8
+        lesson0.hashmap.Employee@43a25848
+        557041912
+        1134712904
+        null
+         */
+    }
+}
